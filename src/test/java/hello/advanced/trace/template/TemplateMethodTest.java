d1592391c8f6a88d0231d1dee4e09c8bb62488f5
dev_one <micool1030@gmail.com>
2023-12-19 20:17:16 +0900

    [test] 템플릿 메서드 패턴 : 익명 내부 클래스 구현

--- src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/trace/template/TemplateMethodTest.java
@@ -47,4 +47,23 @@ class TemplateMethodTest {
         template2.execute();
     }
 
+    @Test
+    void templateMethodeV2() {
+        AbstractTemplate template1 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        template1.execute();
+
+        AbstractTemplate template2 = new AbstractTemplate() {
+            @Override
+            protected void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        template2.execute();
+    }
+
 }
