23fa658c54f9c3c314756ef4054903624d82074a
dev_one <micool1030@gmail.com>
2023-12-21 05:11:56 +0900

    [feat] 템플릿 콜백 패턴 로그 추적기 적용

---
+++ src/main/java/hello/advanced/app/v5/OrderServiceV5.java
@@ -0,0 +1,24 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.callback.TraceTemplate;
+import hello.advanced.trace.logtrace.LogTrace;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class OrderServiceV5 {
+    private final OrderRepositoryV5 orderRepository;
+    private final TraceTemplate traceTemplate;
+
+    public OrderServiceV5(OrderRepositoryV5 orderRepository, LogTrace trace) {
+        this.orderRepository = orderRepository;
+        this.traceTemplate = new TraceTemplate(trace);  // 한번 만들어서 싱글톤으로 사용하기 위해서
+    }
+
+    public void orderItem(String itemId) {
+        traceTemplate.execute("OrderService.orderItem()", () -> {
+            orderRepository.save(itemId);
+            return null;
+        });
+    }
+}
